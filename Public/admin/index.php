59c15455ca7b0273c9bd3e9281fc93e2c28ca8c1
ashmita <46643295+asmi122@users.noreply.github.com>
2020-04-12 15:53:25 +1000

    changes in index file

---
+++ Public/admin/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+    <head>
+        <title> AK </title>
+        <meta charset="utf-8">
+
+    </head>
+
+    <body>
+        <header>
+            <h1> AK Admin  Area</h1>
+        </header>
+
+
+        <navigation>
+            <ul>
+                <li><a href="index.php">Menu</a></li>
+
+            </ul>
+        </navigation>
+
+        <footer>
+            &copy; <?php echo date('y'); ?> Adventure Kora
+        </footer>
+
+        
+    </body>
+
+
+
+</html>

e988308bfcd1a5220b0a6ea33b6b446101ea2a44
ashmita <46643295+asmi122@users.noreply.github.com>
2020-04-12 14:44:27 +1000

    minor changes

---
+++ Public/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Adventure Kora</title>
+        <meta charset="utf-8">
+    </head>
+
+    <body>
+        <h1> Adventure Kora Coming Soon.. </h1>
+    </body>
+</html>

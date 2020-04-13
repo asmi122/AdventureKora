d3eb2bc1f5e60fbe2549c2dfceae653615edf84c
ashmita <46643295+asmi122@users.noreply.github.com>
2020-04-13 15:45:00 +1000

    getting the subjects page of admin work

--- Public/admin/subjects/index.php
+++ Public/admin/subjects/index.php
@@ -1,9 +1,9 @@
-<?php require_once('../../Private/initialize.php'); ?>
+<?php require_once('../../../Private/initialize.php'); ?>
 
-<?php $page_title = 'Admin Menu'; ?>
-<?php include('../../Private/shared/admin_header.php'); ?>
+<?php $page_title = 'Subjects'; ?>
+<?php include('../../../Private/shared/admin_header.php'); ?>
 
 <div id="content">
 </div>
 
-<?php include('../../Private/shared/admin_footer.php'); ?>
+<?php include('../../../Private/shared/admin_footer.php'); ?>

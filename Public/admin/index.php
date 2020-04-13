1ba9ceb28fa8233d7d08c3736f6b9629d1099552
ashmita <46643295+asmi122@users.noreply.github.com>
2020-04-13 15:59:12 +1000

    using constants in index. php of sujects and public folder

--- Public/admin/index.php
+++ Public/admin/index.php
@@ -1,9 +1,9 @@
 <?php require_once('../../Private/initialize.php'); ?>
 
 <?php $page_title = 'Admin Menu'; ?>
-<?php include('../../Private/shared/admin_header.php'); ?>
+<?php include(SHARED_PATH. '/admin_header.php'); ?>
 
 <div id="content">
 </div>
 
-<?php include('../../Private/shared/admin_footer.php'); ?>
+<?php include(SHARED_PATH. '/admin_footer.php'); ?>

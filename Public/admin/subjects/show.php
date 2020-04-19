760d37dc136f31a3c8b4c488b18e61a3efd97c25
ashmita <46643295+asmi122@users.noreply.github.com>
2020-04-19 18:39:34 +1000

    making urlencode and rawurlencode  function in  initialize.php to handle special symbols

---
+++ Public/admin/subjects/show.php
@@ -0,0 +1,13 @@
+<?php require_once('../../../Private/initialize.php'); ?>
+
+<?php
+
+$id = isset($_GET['id'])? $_GET['id']:'1';
+
+echo $id;
+
+?>
+
+<a href="show.php?name=<?php echo u('John Doe');?>">Link</a></br>
+<a href="show.php?company=<?php echo u('Adventure&Kora');?>">Link</a></br>
+<a href="show.php?query=<?php echo u('#$%^');?>">Link</a></br>

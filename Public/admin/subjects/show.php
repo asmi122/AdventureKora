d2d8f30925d4b0d6a0f2ca45e69795a6aa73bb77
ashmita <46643295+asmi122@users.noreply.github.com>
2020-04-23 13:20:50 +1000

    displaying id

--- Public/admin/subjects/show.php
+++ Public/admin/subjects/show.php
@@ -3,11 +3,17 @@
 <?php
 
 $id = isset($_GET['id'])? $_GET['id']:'1';
-
-echo h($id);
-
 ?>
 
-<a href="show.php?name=<?php echo u('John Doe');?>">Link</a></br>
-<a href="show.php?company=<?php echo u('Adventure&Kora');?>">Link</a></br>
-<a href="show.php?query=<?php echo u('#$%^');?>">Link</a></br>
+<?php $page_title = 'Show Subject'; ?>
+<?php include(SHARED_PATH . '/admin_header.php'); ?>
+
+<div id="content">
+
+	<a class="back-link" href="<?php echo url_for('/admin/subjects/index.php');?>">&laquo; Back to List </a>
+
+	<div class="subject show">
+
+		Subject ID: <?php echo h($id); ?>
+	</div>
+</div>

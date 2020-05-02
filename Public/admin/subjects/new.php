e22331368580601d7d49259a56b4b1b0e3a01869
ashmita <46643295+asmi122@users.noreply.github.com>
2020-05-02 17:57:56 +1000

    testing

--- Public/admin/subjects/new.php
+++ Public/admin/subjects/new.php
@@ -1,54 +1,55 @@
-<?php 
+<?php
 
-require_once('../../../Private/initialize.php'); 
+require_once('../../../private/initialize.php');
 
 $test = $_GET['test'] ?? '';
 
 if($test == '404') {
-	error_404();
+  error_404();
 } elseif($test == '500') {
-	error_500();
+  error_500();
 } elseif($test == 'redirect') {
-	redirect_to(url_for('/admin/subjects/index.php'));
+  redirect_to(url_for('/admin/subjects/index.php'));
 }
 ?>
 
-<?php $page_title='Create Subject';?>
-<?php include(SHARED_PATH. '/staff_header.php');?>
+<?php $page_title = 'Create Subject'; ?>
+<?php include(SHARED_PATH . '/admin_header.php'); ?>
 
 <div id="content">
-	<a class="back-link" href="<?php echo url_for('/admin/subjects/index.php'); ?>">&laquo; Back to List </a>
 
-	<div class="subject new">
-		<h1>Create Subject</h1>
+  <a class="back-link" href="<?php echo url_for('/admin/subjects/index.php'); ?>">&laquo; Back to List</a>
 
-		<form action="<?php echo url_for('/admin/subjects/create.php'); ?>" method="post">
-			<dl>
-				<dt>Menu Name</dt>
-				<dd><input type="text" name="menu_name" value="" /></dd>
-			</dl>
-			<dl>
-				<dt>Position</dt>
-				<dd>
-					<select name="Position">
-						<option value="1">1</option>
-					</select>
+  <div class="subject new">
+    <h1>Create Subject</h1>
 
-				</dd>
-			</dl>
-			<dl>
-				<dt>Visible</dt>
-				<dd>
-					<input type="hidden" name="Visible" value="0" />
-					<input type="checkbox" name="Visible" value="1" />
-				</dd>
-			</dl>
-			<div id="operations">
-				<input type="submit" value="Create Subject" />
+    <form action="<?php echo url_for('/admin/subjects/create.php') ?>" method="post">
+      <dl>
+        <dt>Menu Name</dt>
+        <dd><input type="text" name="menu_name" value="" /></dd>
+      </dl>
+      <dl>
+        <dt>Position</dt>
+        <dd>
+          <select name="position">
+            <option value="1">1</option>
+          </select>
+        </dd>
+      </dl>
+      <dl>
+        <dt>Visible</dt>
+        <dd>
+          <input type="hidden" name="visible" value="0" />
+          <input type="checkbox" name="visible" value="1" />
+        </dd>
+      </dl>
+      <div id="operations">
+        <input type="submit" value="Create Subject" />
+      </div>
+    </form>
 
-			</div>
-		</form>
-	</div>
+  </div>
+
 </div>
 
-<?php include(SHARED_PATH. '/admin_footer.php'); ?>
+<?php include(SHARED_PATH . '/admin_footer.php'); ?>

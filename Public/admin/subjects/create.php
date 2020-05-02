30cf7b4b4f2bc550f6a291a073f4ba843d8c21fb
ashmita <46643295+asmi122@users.noreply.github.com>
2020-05-02 18:32:35 +1000

    created function to determine get or post request

--- Public/admin/subjects/create.php
+++ Public/admin/subjects/create.php
@@ -1,14 +1,18 @@
 <?php
+require_once('../../../private/initialize.php');
 
+if(is_post_request()){
 // handle form value sent by new.php
 
-$menu_name = $_POST['menu_name'] ?? '';
-$position = $_POST['position'] ?? '';
-$visible = $_POST['visible'] ?? '';
+    $menu_name = $_POST['menu_name'] ?? '';
+    $position = $_POST['position'] ?? '';
+    $visible = $_POST['visible'] ?? '';
 
-echo "Form parameter<br/>";
-echo "menu_name" . $menu_name . "<br />";
-echo "position" . $position . "<br />";
-echo "visible" . $visible . "<br />";
-
+    echo "Form parameter<br/>";
+    echo "menu_name" . $menu_name . "<br />";
+    echo "position" . $position . "<br />";
+    echo "visible" . $visible . "<br />";
+} else {
+    redirect_to(url_for('/admin/subjects/new.php'));
+}
 ?>

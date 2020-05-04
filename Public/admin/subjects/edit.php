0f4d3326f2a925731ec8ddf4679d7c5557645ecd
ashmita <46643295+asmi122@users.noreply.github.com>
2020-05-04 10:34:15 +1000

    single page form submission

--- Public/admin/subjects/edit.php
+++ Public/admin/subjects/edit.php
@@ -2,53 +2,68 @@
 
 require_once('../../../Private/initialize.php'); 
 
-$test = $_GET['test'] ?? '';
 
-if($test == '404') {
-	error_404();
-} elseif($test == '500') {
-	error_500();
-}elseif ($test == 'redirect') {
-	redirect_to(url_for('/admin/subjects/index.php'));
+if(!isset($_GET['id'])) {
+  redirect_to(url_for('/admin/subjects/index.php'));
 }
+$id = $_GET['id'];
+$menu_name = '';
+$position = '';
+$visible = '';
+
+if(is_post_request()) {
+
+  // Handle form values sent by new.php
+
+  $menu_name = $_POST['menu_name'] ?? '';
+  $position = $_POST['position'] ?? '';
+  $visible = $_POST['visible'] ?? '';
+
+  echo "Form parameters<br />";
+  echo "Menu name: " . $menu_name . "<br />";
+  echo "Position: " . $position . "<br />";
+  echo "Visible: " . $visible . "<br />";
+}
+
 ?>
 
 <?php $page_title = 'Edit Subject'; ?>
-<?php include(SHARED_PATH. '/admin_header.php');?>
+<?php include(SHARED_PATH . '/admin_header.php'); ?>
 
 <div id="content">
-	<a class="back-link" href="<?php echo url_for('/admin/subjects/index.php'); ?>">&laquo; Back to List </a>
 
-	<div class="subject edit">
-		<h1>Edit Subject</h1>
+  <a class="back-link" href="<?php echo url_for('/admin/subjects/index.php'); ?>">&laquo; Back to List</a>
 
-		<form action="" method="post">
-			<dl>
-				<dt>Menu Name</dt>
-				<dd><input type="text" name="menu_name" value="" /></dd>
-			</dl>
-			<dl>
-				<dt>Position</dt>
-				<dd>
-					<select name="Position">
-						<option value="1">1</option>
-					</select>
+  <div class="subject edit">
+    <h1>Edit Subject</h1>
 
-				</dd>
-			</dl>
-			<dl>
-				<dt>Visible</dt>
-				<dd>
-					<input type="hidden" name="Visible" value="0" />
-					<input type="checkbox" name="Visible" value="1" />
-				</dd>
-			</dl>
-			<div id="operations">
-				<input type="Edit" value="Create Subject" />
+    <form action="<?php echo url_for('/admin/subjects/edit.php?id=' . h(u($id))); ?>" method="post">
+      <dl>
+        <dt>Menu Name</dt>
+        <dd><input type="text" name="menu_name" value="<?php echo $menu_name; ?>" /></dd>
+      </dl>
+      <dl>
+        <dt>Position</dt>
+        <dd>
+          <select name="position">
+            <option value="1">1</option>
+          </select>
+        </dd>
+      </dl>
+      <dl>
+        <dt>Visible</dt>
+        <dd>
+          <input type="hidden" name="visible" value="0" />
+          <input type="checkbox" name="visible" value="1" />
+        </dd>
+      </dl>
+      <div id="operations">
+        <input type="submit" value="Edit Subject" />
+      </div>
+    </form>
 
-			</div>
-		</form>
-	</div>
+  </div>
+
 </div>
 
-<?php include(SHARED_PATH. '/admin_footer.php'); ?>
+<?php include(SHARED_PATH . '/admin_footer.php'); ?>

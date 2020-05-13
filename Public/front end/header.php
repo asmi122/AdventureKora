e3774bd9698f640df0f59dc2c5d0fb76345109bb
Manpreet Braich <manpreetbenipal07@gmail.com>
2020-05-14 08:43:36 +1000

    setting up my local envirnment

---
+++ Public/front end/header.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Adventure Kora</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Custom styles for this template -->
+  <link href="css/mystyle.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="body-inner">
+
+		<div id="top-bar" class="top-bar">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-8 col-md-8 col-sm-6 col-xs-12">
+						<ul class="top-info">
+							<li><i class="fa fa-map-marker">&nbsp;</i>
+								<p class="info-text"> Kathmandu, Nepal</p>
+							</li>
+						</ul>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 top-social text-right">
+						<ul class="unstyled">
+						<li>
+                            <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-tripadvisor" aria-hidden="true"></i></a>
+							</li>
+							</ul>
+                    </div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Header start -->
+		<header id="header" class="header-one">
+			<div class="container">
+				<div class="row">
+					<div class="logo-area clearfix">
+						<div class="logo col-xs-12 col-md-3">
+							<a href="#">
+								<img src="images/logo.png" alt="">
+							</a>
+						</div><!-- logo end -->
+
+						<div class="col-xs-12 col-md-9 header-right">
+							<ul class="top-info-box">
+							<li>
+									<div class="info-box">
+										<div class="info-box-content">
+											<p class="info-box-title">Enquery Hours</p>
+											<p class="info-box-subtitle">Mon-Sat 9am - 6pm</p>
+											
+										</div>
+									</div>
+								</li>
+								<li>
+									<div class="info-box">
+										<div class="info-box-content">
+											<p class="info-box-title">Call Us</p>
+											<p class="info-box-subtitle">+9779849590899</p>
+										</div>
+									</div>
+								</li>
+								<li>
+									<div class="info-box">
+										<div class="info-box-content">
+											<p class="info-box-title">Email Us</p>
+											<p class="info-box-subtitle">bhandari039@gmail.com</p>
+										</div>
+									</div>
+								</li>
+								
+								
+							</ul><!-- Ul end -->
+						</div><!-- header right end -->
+					</div><!-- logo area end -->
+
+				</div><!-- Row end -->
+			</div><!-- Container end -->
+
+		<nav class="site-navigation navigation navdown">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12">
+							<div class="site-nav-inner pull-left">
+								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+
+								<div class="collapse navbar-collapse navbar-responsive-collapse">
+									<ul class="nav navbar-nav">
+										<li class="dropdown active">
+										<a href="#">Homes</a></li>
+
+										<li class="dropdown">
+											<a href="#" class="dropdown-toggle" data-toggle="dropdown">Trip Type <i
+													class="fa fa-angle-down"></i></a>
+											<ul class="dropdown-menu" role="menu">
+												<li><a href="#">Trekking</a></li>
+												<li><a href="#">Peak Climbing</a></li>
+												<li><a href="#">Tours</a></li>
+												<li><a href="#">Adventures Sports</a></li>
+												<li><a href="#">Volunteering</a></li>
+											</ul>
+										</li>
+
+										
+
+										<li><a href="#">Tailor-Made</a></li>
+
+										<li class="dropdown">
+											<a href="#" class="dropdown-toggle" data-toggle="dropdown">Plan a Trip<i
+													class="fa fa-angle-down"></i></a>
+											<ul class="dropdown-menu" role="menu">
+												<li><a href="#">Book Now</a></li>
+												<li><a href="#">Join Pool</a></li>
+												
+											</ul>
+										</li>
+
+										<li class="dropdown">
+											<a href="#" class="dropdown-toggle" data-toggle="dropdown">About Us<i
+													class="fa fa-angle-down"></i></a>
+											<ul class="dropdown-menu" role="menu">
+												<li><a href="#">Who we are</a></li>
+												<li><a href="#">Why Adventure Kora</a></li>
+												<li><a href="#">Our Story</a></li>
+												<li><a href="#">Our Team</a></li>
+											</ul>
+										</li>
+
+										<li><a href="#">Contact Us</a></li>
+
+									</ul>
+									<!--/ Nav ul end -->
+								</div>
+								<!--/ Collapse end -->
+
+							</div><!-- Site Navbar inner end -->
+
+						</div>
+						<!--/ Col end -->
+					</div>
+					<!--/ Row end -->
+
+					<div class="nav-search">
+						<span id="search"><i class="fa fa-search"></i></span>
+					</div><!-- Search end -->
+
+					<div class="search-block" style="display: none;">
+						<input type="text" class="form-control" placeholder="Type what you want and enter">
+						<span class="search-close">&times;</span>
+					</div><!-- Site search end -->
+				</div>
+				<!--/ Container end -->
+
+			</nav>
+			<!--/ Navigation end -->
+		</header>
+
+
+
+      
+</body>
+</html> 

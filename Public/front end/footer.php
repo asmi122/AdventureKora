1c34fa5008a9b38043e0ade8c502b2e4050782c0
Manpreet Braich <manpreetbenipal07@gmail.com>
2020-05-15 08:39:41 +1000

    Footer with some changes in CSS

---
+++ Public/front end/footer.php
@@ -0,0 +1,93 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Adventure Kora</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Custom styles for this template -->
+  <link href="css/mystyle.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+ <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6">
+            <div class="footer-info">
+              <h3>Adventure Kora</h3>
+              <p>
+                 <hr class="fa fa-map-marker">&nbsp;</hr <br>
+                Kathmandu, Nepal<br><br>
+                <strong>Phone:</strong> +9779849590899<br>
+                <strong>Email:</strong> bhandari039@gmail.com<br>
+              </p>
+              <div class="social-links mt-3">
+                <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-tripadvisor" aria-hidden="true"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Contact us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">FAQs</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Testimonials</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms & Condotions</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Trekking</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Peak Climbing</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Tours</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Adventures Sports</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Volunteering</a></li>
+            </ul>
+          </div>
+		   <div class="col-lg-4 col-md-6">
+            <div class="footer-info">
+              <h3>Partners</h3>
+              <img src="images/footer3.png"> <img src="images/footer2.png"> <img src="images/footer1.png"><br>
+			  <a href="#"><button type="button" class="btn btn-primary">Plan Your Trip</button></a>
+			
+            </div>
+          </div>
+
+          
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>2020 - Adventure Kora</span></strong>. All Rights Reserved
+      </div>
+     
+    </div>
+  </footer><!-- End Footer -->
+		</body>
+		</html>

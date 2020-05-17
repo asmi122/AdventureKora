835c04e9b2aa788f4c43f71e3595dbf24188b90f
Manpreet Braich <manpreetbenipal07@gmail.com>
2020-05-17 23:01:18 +1000

    new formatting of header and footers

--- Public/front end/footer.php
+++ Public/front end/footer.php
@@ -1,30 +1,46 @@
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<head>
-
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>Adventure Kora</title>
-
-  <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Custom styles for this template -->
-  <link href="css/mystyle.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-</head>
-<body>
- <!-- ======= Footer ======= -->
+    <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/mystyle.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <title>Adventure Kora</title>
+  </head>
+  <body>
+  <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+  <!-- ======= Footer ======= -->
+  <div class="company-partners">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm" style="align:right">
+        
+                </div>
+    <div class="col-sm" style="padding-left: 80px;">
+  <a href="#"><img src="images/footer1.png"></a>
+      <a href="#"><img src="images/footer3.png"></a>
+    <a href="#"><img src="images/footer2.png"></a>
+    </div>
+    <div class="col-sm" style="align:left">
+      
+    </div>
+   </div>
+   </div>
+   <div>
   <footer id="footer">
     <div class="footer-top">
       <div class="container">
+    
         <div class="row">
 
           <div class="col-lg-4 col-md-6">
@@ -37,16 +53,16 @@
                 <strong>Email:</strong> bhandari039@gmail.com<br>
               </p>
               <div class="social-links mt-3">
-                <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                            <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                            <a href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a>
-                            <a href="#"><i class="fa fa-tripadvisor" aria-hidden="true"></i></a>
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-youtube"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+        <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-tripadvisor"></i></a>
               </div>
             </div>
           </div>
 
-          <div class="col-lg-2 col-md-6 footer-links">
+          <div class="col-lg-4 col-md-6 footer-links" style="padding-left: 100px;">
             <h4>Useful Links</h4>
             <ul>
               <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
@@ -57,7 +73,7 @@
             </ul>
           </div>
 
-          <div class="col-lg-2 col-md-6 footer-links">
+          <div class="col-lg-4 col-md-6 footer-links">
             <h4>Our Services</h4>
             <ul>
               <li><i class="bx bx-chevron-right"></i> <a href="#">Trekking</a></li>
@@ -67,14 +83,7 @@
               <li><i class="bx bx-chevron-right"></i> <a href="#">Volunteering</a></li>
             </ul>
           </div>
-		   <div class="col-lg-4 col-md-6">
-            <div class="footer-info">
-              <h3>Partners</h3>
-              <img src="images/footer3.png"> <img src="images/footer2.png"> <img src="images/footer1.png"><br>
-			  <a href="#"><button type="button" class="btn btn-primary">Plan Your Trip</button></a>
-			
-            </div>
-          </div>
+       
 
           
 
@@ -89,5 +98,4 @@
      
     </div>
   </footer><!-- End Footer -->
-		</body>
-		</html>
+</html>

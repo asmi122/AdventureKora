23d551f7c57a370342f700944b6bd226114bf879
Jasvi-Chahal <58879701+Jasvi-Chahal@users.noreply.github.com>
2020-05-25 12:16:42 +1000

    Add files via upload

---
+++ PUBLIC/HeaderFooter/header.php
@@ -0,0 +1,136 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/mystyle.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <!-----Owl-Carousel----->
+    <link rel="stylesheet" href="./CSS/owl.carousel.min.css">
+    <link rel="stylesheet" href="./CSS/owl.theme.default.min.css">
+    <title>Adventure Kora</title>
+  </head>
+  <body>
+   <!------------- Top header navbar----------->
+   
+   <div class="top-bar">
+        <div class="container">
+          <div class="row d-flex align-items-center">
+            <div class="col-md-6 d-md-block d-none">
+			  <i class="fas fa-map-marker-alt">  Kathmandu, Nepal</i>
+              
+            </div>
+            <div class="col-md-6">
+              <div class="d-flex justify-content-md-end justify-content-between">
+                <ul class="social-custom list-inline">
+                  <li class="list-inline-item"><a href="https://www.facebook.com/adventurekora"><i class="fab fa-facebook-f"></i></a></li>
+                  <li class="list-inline-item"><a href="https://www.youtube.com/channel/UCfKnZINCSigmM7Rdtt7Qd_w"><i class="fab fa-youtube"></i></a></li>
+                  <li class="list-inline-item"><a href="https://twitter.com/home"><i class="fab fa-twitter"></i></a></li>
+				         <li class="list-inline-item"><a href="https://www.instagram.com/adventurekora/"><i class="fab fa-instagram"></i></a></li>
+                  <li class="list-inline-item"><a href="https://www.tripadvisor.com.au/Attraction_Review-g293890-d13795234-Reviews-Adventure_Kora-Kathmandu_Kathmandu_Valley_Bagmati_Zone_Central_Region.html"><i class="fab fa-tripadvisor"></i></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  <!----------------- Logo-------------------->
+  
+	<div class="company-logo">
+		<div class="container">
+			<div class="row d-flex align-items-center"><hr>
+            <div class="col-md-6 d-md-block d-none">
+              <a href="index.php"><img src="images/logo.png"></a>
+            </div>
+            <div class="col-md-6">
+              <div class="d-flex justify-content-md-end justify-content-between">
+                <div class="info-box">
+					<div class="info-box-content" style="border-right-style: solid;border-right-width: 2px;padding-right: 10px;">
+						<p class="info-box-title"><i class="far fa-clock"></i>  Enquery Hours</p>
+						<p class="info-box-subtitle">Mon-Sat 9am - 6pm</p>
+					</div>
+				</div>
+				<div class="info-box">
+					<div class="info-box-content" style="border-right-style: solid;border-right-width: 2px;padding-right: 10px; padding-left:10px;">
+						<p class="info-box-title"><i class="fas fa-phone"></i>  Call Us</p>
+						<p class="info-box-subtitle">+9779849590899</p>
+					</div>
+				</div>
+				<div class="info-box">
+					<div class="info-box-content"style="padding-left:10px;">
+						<p class="info-box-title"><i class="fas fa-envelope"></i>  Email Us</p>
+						<p class="info-box-subtitle">adventurekora@gmail.com</p>
+					</div>
+				</div>
+				</div>
+            </div>				
+          </div>
+		</div>
+	</div>
+	<!-------------------navigation bar---------------------->
+	<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <!-- Brand -->
+        <a class="navbar" href="index.php"><img src="images/kora.png"></a>
+		  <!-- Links -->
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                   <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Trip Type </a>
+						<div class="dropdown-menu">
+							<a href="#">Trekking</a>
+							<a href="#">Peak Climbing</a>
+							<a href="#">Tours</a>
+							<a href="#">Adventures Sports</a>
+							<a href="#">Volunteering</a>
+						</div>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="TailorMadeEnquiry.php">Tailor Made</a>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="EnquiryTailorMade.php" id="navbardrop" data-toggle="dropdown"> Plan A Trip </a>
+					<div class="dropdown-menu">
+						<a href="TailorMadeEnquiry.php">Book Now</a>
+						<a href="#">Join Pool</a> </div>
+                </li>
+	            <li class="nav-item dropdown">
+                     <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown"> About Us </a>
+						<div class="dropdown-menu">
+							<a href="WhoWeAre.php">Who we are</a>
+							<a href="WhyAdventureKora.php">Why Adventure Kora</a>
+							<a href="OurStory.php">Our Story</a>
+							<a href="OurTeam.php">Our Team</a>
+						</div>
+				</li>
+	            <li class="nav-item">
+                    <a class="nav-link" href="ContactUs.php">Contact Us</a>
+                </li>
+				<div class="search-container">
+                   <form action="/action_page.php">
+                       <input type="text" placeholder="Search.." name="search">
+                       <button type="submit"><i class="fa fa-search"></i></button>
+                    </form>
+                </div>
+            </ul>
+            
+	</nav><!----------nav end--------->
+  
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <!-----Owl-Carousel----->
+    <script src="./JS/owl.carousel.min.js"></script>
+  </body>
+</html>
